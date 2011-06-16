abf81d8a06250fb9f346f66173bd525d5ac4cc12
Ben Wilhelm <ben@doublebeamdesign.com>
2011-06-15 19:58:41 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Frisbot::Application.config.secret_token = '23b7de4dfe75bef12462cbafb2e8849fb9871163a86927229536fe6145c3fb9fa5ee19755f08a83bd9c13382cfc56aa2c51667fbdc6b3e6cf9d271b5b5bec0fe'
