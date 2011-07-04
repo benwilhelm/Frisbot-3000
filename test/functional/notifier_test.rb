2cf1e3459186bf00a1a8b10b7f92f910cfc9d122
Ben Wilhelm <ben@doublebeamdesign.com>
2011-07-03 17:44:17 -0700

    Initial Email functional test

--- test/functional/notifier_test.rb
+++ test/functional/notifier_test.rb
@@ -2,19 +2,19 @@ require 'test_helper'
 
 class NotifierTest < ActionMailer::TestCase
   test "game_created" do
-    mail = Notifier.game_created
-    assert_equal "Game created", mail.subject
-    assert_equal ["to@example.org"], mail.to
-    assert_equal ["from@example.com"], mail.from
-    assert_match "Hi", mail.body.encoded
-  end
-
-  test "game_updated" do
-    mail = Notifier.game_updated
-    assert_equal "Game updated", mail.subject
-    assert_equal ["to@example.org"], mail.to
-    assert_equal ["from@example.com"], mail.from
-    assert_match "Hi", mail.body.encoded
+    @rsvp = rsvps(:one)
+    @game = games(:one)
+    @user = User.find(@rsvp.user_id)
+    
+    mail = Notifier.game_created(@rsvp)
+    assert_equal "New Ultimate Game: " + @game.game_time.to_s(:game_time_long), mail.subject
+    assert_equal [@user.email], mail.to
+    assert_equal ["games@frisbot3000.com"], mail.from
+    
+    yes_url = "http://localhost:3000/rsvps/" + @rsvp.id.to_s + "/respond/Y/" + @rsvp.auth_token
+    no_url = "http://localhost:3000/rsvps/" + @rsvp.id.to_s + "/respond/N/" + @rsvp.auth_token
+    assert_match yes_url, mail.body.encoded
+    assert_match no_url, mail.body.encoded
   end
 
 end
