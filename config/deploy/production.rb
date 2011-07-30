ea622093ff34a9b3bc92a0979c48719f32bb6043
Ben Wilhelm <ben@doublebeamdesign.com>
2011-07-30 10:42:36 -0700

    production deploys to frisbot, not staging

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,6 +3,6 @@ set :branch, "release"
 
 set :deploy_to, "/home/snoopy/public_html/#{application}"
 
-role :web, "staging.frisbot3000.com", :port => 22000
-role :app, "staging.frisbot3000.com", :port => 22000
-role :db,  "staging.frisbot3000.com", :port => 22000, :primary => true # This is where Rails migrations will run
+role :web, "frisbot3000.com", :port => 22000
+role :app, "frisbot3000.com", :port => 22000
+role :db,  "frisbot3000.com", :port => 22000, :primary => true # This is where Rails migrations will run
