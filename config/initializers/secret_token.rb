792fbf8f5d273912e2c1faacc637912d5b6aa5b8
Ben Wilhelm <ben@doublebeamdesign.com>
2011-08-02 15:19:42 -0700

    Rails update

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Frisbot::Application.config.secret_token = '23b7de4dfe75bef12462cbafb2e8849fb9871163a86927229536fe6145c3fb9fa5ee19755f08a83bd9c13382cfc56aa2c51667fbdc6b3e6cf9d271b5b5bec0fe'
+Frisbot::Application.config.secret_token = '8c2d611f37387013f5d586ed55c30099d139b3609b3888c40cea047a68baee9a666b80199c12010d070c336aaeb824714abe3c8145f41986234515a9b1c643eb'
