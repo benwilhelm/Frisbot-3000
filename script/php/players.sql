57c4cf44255242981f28b692066f65f2221137e2
Ben Wilhelm <ben@doublebeamdesign.com>
2011-08-05 22:08:19 -0700

    Insert .sql for old players

---
+++ script/php/players.sql
@@ -0,0 +1,87 @@
+-- phpMyAdmin SQL Dump
+-- version 3.3.10
+-- http://www.phpmyadmin.net
+--
+-- Host: mysql.benwilhelm.com
+-- Generation Time: Aug 05, 2011 at 09:30 PM
+-- Server version: 5.1.39
+-- PHP Version: 5.2.17
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `frisbeecollective`
+--
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`email`, `fname`, `lname`, `encrypted_password`, `admin`) VALUES('aaronmpreusse@gmail.com', 'Aaron', 'Preusse', 'woeinvljlwkes', 0);
+INSERT INTO `users` (`email`, `fname`, `lname`, `encrypted_password`, `admin`) VALUES('adanzig1@mac.com', 'Adrian', 'Danzig', 'woeinvljlwkes', 0);
+INSERT INTO `users` (`email`, `fname`, `lname`, `encrypted_password`, `admin`) VALUES('amesblair@yahoo.com', 'Amy', 'Blair', 'woeinvljlwkes', 0);
+INSERT INTO `users` (`email`, `fname`, `lname`, `encrypted_password`, `admin`) VALUES('artworksdesign@ameritech.net', 'Jason', 'Greenberg', 'woeinvljlwkes', 0);
+INSERT INTO `users` (`email`, `fname`, `lname`, `encrypted_password`, `admin`) VALUES('bdesgranges@hotmail.com', 'Brian', 'DesGranges', 'woeinvljlwkes', 0);
+INSERT INTO `users` (`email`, `fname`, `lname`, `encrypted_password`, `admin`) VALUES('ben@thehousetheatre.com', 'Ben', 'Lobpries', 'woeinvljlwkes', 0);
+INSERT INTO `users` (`email`, `fname`, `lname`, `encrypted_password`, `admin`) VALUES('benjamindicke@gmail.com', 'Ben', 'Dicke', 'woeinvljlwkes', 0);
+INSERT INTO `users` (`email`, `fname`, `lname`, `encrypted_password`, `admin`) VALUES('brak0999@gmail.com', 'Sean', 'O''Brien', '312-733-5802', 1);
+INSERT INTO `users` (`email`, `fname`, `lname`, `encrypted_password`, `admin`) VALUES('carly.shapiro25@gmail.com', 'Carly', 'Shapiro', 'woeinvljlwkes', 0);
+INSERT INTO `users` (`email`, `fname`, `lname`, `encrypted_password`, `admin`) VALUES('centron@comtrad.net', 'Matthew', 'Johnson', 'woeinvljlwkes', 0);
+INSERT INTO `users` (`email`, `fname`, `lname`, `encrypted_password`, `admin`) VALUES('chels.meyers@gmail.com', 'chelsea', 'meyers', 'woeinvljlwkes', 0);
+INSERT INTO `users` (`email`, `fname`, `lname`, `encrypted_password`, `admin`) VALUES('chris@chrisburnham.com', 'Chris', 'Burnham', 'woeinvljlwkes', 0);
+INSERT INTO `users` (`email`, `fname`, `lname`, `encrypted_password`, `admin`) VALUES('chrisplevs@gmail.com', 'Chris', 'Plevin', 'woeinvljlwkes', 0);
+INSERT INTO `users` (`email`, `fname`, `lname`, `encrypted_password`, `admin`) VALUES('collins@intergreen.org', 'matthew', 'collins', 'woeinvljlwkes', 0);
+INSERT INTO `users` (`email`, `fname`, `lname`, `encrypted_password`, `admin`) VALUES('cornish.dpt@comcast.net', 'Cory', 'Block', 'woeinvljlwkes', 0);
+INSERT INTO `users` (`email`, `fname`, `lname`, `encrypted_password`, `admin`) VALUES('crcdawg@aol.com', 'Cliff', 'Chamberlain', 'woeinvljlwkes', 0);
+INSERT INTO `users` (`email`, `fname`, `lname`, `encrypted_password`, `admin`) VALUES('dan.price@mac.com', 'Dan', 'Price', 'woeinvljlwkes', 0);
+INSERT INTO `users` (`email`, `fname`, `lname`, `encrypted_password`, `admin`) VALUES('dbaer1003@gmail.com', 'debbie', 'baer', 'woeinvljlwkes', 0);
+INSERT INTO `users` (`email`, `fname`, `lname`, `encrypted_password`, `admin`) VALUES('denniswatkins@thehousetheatre.com', 'Dennis', 'Watkins', 'woeinvljlwkes', 0);
+INSERT INTO `users` (`email`, `fname`, `lname`, `encrypted_password`, `admin`) VALUES('derek.r.hammer@gmail.com', 'Derek', 'Hammer', 'woeinvljlwkes', 0);
+INSERT INTO `users` (`email`, `fname`, `lname`, `encrypted_password`, `admin`) VALUES('dyer_v@hotmail.com', 'Dyer', 'Vann', 'woeinvljlwkes', 0);
+INSERT INTO `users` (`email`, `fname`, `lname`, `encrypted_password`, `admin`) VALUES('E.ejc@Verizon.net', 'Erica', 'Collins', 'woeinvljlwkes', 0);
+INSERT INTO `users` (`email`, `fname`, `lname`, `encrypted_password`, `admin`) VALUES('estadenton@gmail.com', 'Esta', 'Denton', 'woeinvljlwkes', 0);
+INSERT INTO `users` (`email`, `fname`, `lname`, `encrypted_password`, `admin`) VALUES('evankardon@gmail.com', 'evan', 'kardon', 'woeinvljlwkes', 0);
+INSERT INTO `users` (`email`, `fname`, `lname`, `encrypted_password`, `admin`) VALUES('Fordreynolds@gmail.com', 'Robert', 'Ford', 'woeinvljlwkes', 0);
+INSERT INTO `users` (`email`, `fname`, `lname`, `encrypted_password`, `admin`) VALUES('jamieabelson@gmail.com', 'Jamie', 'Abelson', 'woeinvljlwkes', 0);
+INSERT INTO `users` (`email`, `fname`, `lname`, `encrypted_password`, `admin`) VALUES('jenny.l.frey@gmail.com', 'Jenny', 'Frey', 'woeinvljlwkes', 0);
+INSERT INTO `users` (`email`, `fname`, `lname`, `encrypted_password`, `admin`) VALUES('jerry.e.vandyk@gmail.com', 'Jerry', 'Bolf Van Dyk', 'woeinvljlwkes', 0);
+INSERT INTO `users` (`email`, `fname`, `lname`, `encrypted_password`, `admin`) VALUES('jerrysbauer@gmail.com', 'Jerr', 'Bauer', 'woeinvljlwkes', 0);
+INSERT INTO `users` (`email`, `fname`, `lname`, `encrypted_password`, `admin`) VALUES('jgadda@gmail.com', 'Jen', 'Gadda', 'woeinvljlwkes', 0);
+INSERT INTO `users` (`email`, `fname`, `lname`, `encrypted_password`, `admin`) VALUES('jgreenb4@depaul.edu', 'Jason', 'Greenberg', 'woeinvljlwkes', 0);
+INSERT INTO `users` (`email`, `fname`, `lname`, `encrypted_password`, `admin`) VALUES('jimmoore@cityofchicago.org', 'Jim', 'Moore', 'woeinvljlwkes', 0);
+INSERT INTO `users` (`email`, `fname`, `lname`, `encrypted_password`, `admin`) VALUES('jrnook@gmail.com', 'Jon', 'Nook', 'woeinvljlwkes', 0);
+INSERT INTO `users` (`email`, `fname`, `lname`, `encrypted_password`, `admin`) VALUES('kevin.kingsley@ou.edu', 'Kevin', 'Kingsley', 'woeinvljlwkes', 0);
+INSERT INTO `users` (`email`, `fname`, `lname`, `encrypted_password`, `admin`) VALUES('keysclown@aol.com', 'keeland', 'scher', 'woeinvljlwkes', 0);
+INSERT INTO `users` (`email`, `fname`, `lname`, `encrypted_password`, `admin`) VALUES('Kimberly.Weed@gmail.com', 'Kim', 'Weed', 'woeinvljlwkes', 0);
+INSERT INTO `users` (`email`, `fname`, `lname`, `encrypted_password`, `admin`) VALUES('klitewka@gmail.com', 'Chris', 'Litewka', 'woeinvljlwkes', 0);
+INSERT INTO `users` (`email`, `fname`, `lname`, `encrypted_password`, `admin`) VALUES('lloydperlow@yahoo.com', 'Lloyd ', 'Perlow', 'woeinvljlwkes', 0);
+INSERT INTO `users` (`email`, `fname`, `lname`, `encrypted_password`, `admin`) VALUES('lowell.wyse@gmail.com', 'Lowell', 'Wyse', 'woeinvljlwkes', 0);
+INSERT INTO `users` (`email`, `fname`, `lname`, `encrypted_password`, `admin`) VALUES('LPerlow@atg.state.il.us', 'Lloyd', 'Perlow', 'woeinvljlwkes', 0);
+INSERT INTO `users` (`email`, `fname`, `lname`, `encrypted_password`, `admin`) VALUES('lstuber@luc.edu', 'Leann', 'Stuber', 'woeinvljlwkes', 0);
+INSERT INTO `users` (`email`, `fname`, `lname`, `encrypted_password`, `admin`) VALUES('lucasmerino@gmail.com', 'Lucas', 'Merino', 'woeinvljlwkes', 0);
+INSERT INTO `users` (`email`, `fname`, `lname`, `encrypted_password`, `admin`) VALUES('marywinn@gmail.com', 'Mary Winn', 'Heider', 'woeinvljlwkes', 0);
+INSERT INTO `users` (`email`, `fname`, `lname`, `encrypted_password`, `admin`) VALUES('mattboze@yahoo.com', 'matt', 'bozeday', 'woeinvljlwkes', 0);
+INSERT INTO `users` (`email`, `fname`, `lname`, `encrypted_password`, `admin`) VALUES('michelle@newleaftheatre.org', 'Michelle', 'O''Brien', 'woeinvljlwkes', 0);
+INSERT INTO `users` (`email`, `fname`, `lname`, `encrypted_password`, `admin`) VALUES('moconnell426@yahoo.com', 'Mike', 'OConnell', 'woeinvljlwkes', 0);
+INSERT INTO `users` (`email`, `fname`, `lname`, `encrypted_password`, `admin`) VALUES('negina7@gmail.com', 'Negina', 'Kolesar', 'woeinvljlwkes', 0);
+INSERT INTO `users` (`email`, `fname`, `lname`, `encrypted_password`, `admin`) VALUES('patdorsey@gmail.com', 'Pat', 'Dorsey', 'woeinvljlwkes', 0);
+INSERT INTO `users` (`email`, `fname`, `lname`, `encrypted_password`, `admin`) VALUES('pegordon@gmail.com', 'Peter', 'Gordon', 'woeinvljlwkes', 0);
+INSERT INTO `users` (`email`, `fname`, `lname`, `encrypted_password`, `admin`) VALUES('pfagen@gmail.com', 'Paul', 'Fagen', 'woeinvljlwkes', 0);
+INSERT INTO `users` (`email`, `fname`, `lname`, `encrypted_password`, `admin`) VALUES('pfautsch@gmail.com', 'Shawn', 'Pfautsch', 'woeinvljlwkes', 0);
+INSERT INTO `users` (`email`, `fname`, `lname`, `encrypted_password`, `admin`) VALUES('rich@rss-culaw.cm', 'Richard', 'Schulman', 'woeinvljlwkes', 0);
+INSERT INTO `users` (`email`, `fname`, `lname`, `encrypted_password`, `admin`) VALUES('robster72411@aol.com', 'robert', 'Raymond', 'woeinvljlwkes', 0);
+INSERT INTO `users` (`email`, `fname`, `lname`, `encrypted_password`, `admin`) VALUES('sarah@comtrad.net', 'Sarah', 'Johnson', 'woeinvljlwkes', 0);
+INSERT INTO `users` (`email`, `fname`, `lname`, `encrypted_password`, `admin`) VALUES('scotty@scottyiseri.com', 'Scotty', 'Iseri', 'woeinvljlwkes', 0);
+INSERT INTO `users` (`email`, `fname`, `lname`, `encrypted_password`, `admin`) VALUES('toddgaffney@gmail.com', 'Todd', 'Gaffney', 'woeinvljlwkes', 0);
+INSERT INTO `users` (`email`, `fname`, `lname`, `encrypted_password`, `admin`) VALUES('tommy@thehousetheatre.com', 'Tommy', 'Rapley', 'woeinvljlwkes', 0);
+INSERT INTO `users` (`email`, `fname`, `lname`, `encrypted_password`, `admin`) VALUES('Tony.c.mack@gmail.com', 'Tony', 'Mack', 'woeinvljlwkes', 0);
+INSERT INTO `users` (`email`, `fname`, `lname`, `encrypted_password`, `admin`) VALUES('tynancronin@gmail.com', 'Matt', 'Cronin', 'woeinvljlwkes', 0);
+INSERT INTO `users` (`email`, `fname`, `lname`, `encrypted_password`, `admin`) VALUES('victoria.bolf@gmail.com', 'Vicki', 'Bolf', 'woeinvljlwkes', 0);
+INSERT INTO `users` (`email`, `fname`, `lname`, `encrypted_password`, `admin`) VALUES('wilsonsteven@hotmail.com', 'Steve', 'Wilson', 'woeinvljlwkes', 0);
+INSERT INTO `users` (`email`, `fname`, `lname`, `encrypted_password`, `admin`) VALUES('yonibrad@yahoo.com', 'Yoni', 'Pizer', 'woeinvljlwkes', 0);
